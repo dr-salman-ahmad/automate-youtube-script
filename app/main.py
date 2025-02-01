42d5fd5f8ffe3f76b0d2d19bdeef509ef405532b
Salman Ahmad <ssalmanmughal73@gmail.com>
2025-02-01 19:46:51 +0500

    add the jinja template

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from .routers import transcription
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/")
-async def root():
-    return {"message": "It is the root page"}
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
